272a4837aec3593c10b1931b9ff2d4d3c78c0768
Ryo Ota <nwtgck@gmail.com>
2018-09-20 20:36:55 +0900

    [test] Add tests of multi receivers

--- test/piping.test.ts
+++ test/piping.test.ts
@@ -75,4 +75,66 @@ describe('piping.Server', () => {
     // Close the piping server
     await closePromise(pipingServer);
   });
+
+  it('should allow a sender and multi receivers to connect in this order', async () => {
+
+    const pipingPort   = 8787;
+    const pipingServer = http.createServer(new piping.Server().handler);
+    const pipingUrl    = `http://localhost:${pipingPort}`;
+
+    // Listen on the port
+    await listenPromise(pipingServer, pipingPort);
+
+    // Send data
+    // (NOTE: Should use `await`)
+    thenRequest("POST", `${pipingUrl}/mydataid?n=3`, {
+      body: "this is a content"
+    });
+
+    // Get data
+    const dataPromise1 = thenRequest("GET", `${pipingUrl}/mydataid`);
+    const dataPromise2 = thenRequest("GET", `${pipingUrl}/mydataid`);
+    const dataPromise3 = thenRequest("GET", `${pipingUrl}/mydataid`);
+
+    // Await all data
+    const [data1, data2, data3] = await Promise.all([dataPromise1, dataPromise2, dataPromise3]);
+
+    // Body should be the sent data
+    assert.equal(data1.body, "this is a content");
+    assert.equal(data2.body, "this is a content");
+    assert.equal(data3.body, "this is a content");
+
+    // Close the piping server
+    await closePromise(pipingServer);
+  });
+
+  it('should allow multi receivers and a sender to connect in this order', async () => {
+    const pipingPort   = 8787;
+    const pipingServer = http.createServer(new piping.Server().handler);
+    const pipingUrl    = `http://localhost:${pipingPort}`;
+
+    // Listen on the port
+    await listenPromise(pipingServer, pipingPort);
+
+    // Get request promise
+    const dataPromise1 = thenRequest("GET", `${pipingUrl}/mydataid`);
+    const dataPromise2 = thenRequest("GET", `${pipingUrl}/mydataid`);
+    const dataPromise3 = thenRequest("GET", `${pipingUrl}/mydataid`);
+
+    // Send data
+    await thenRequest("POST", `${pipingUrl}/mydataid?n=3`, {
+      body: "this is a content"
+    });
+
+    // Await all data
+    const [data1, data2, data3] = await Promise.all([dataPromise1, dataPromise2, dataPromise3]);
+
+    // Body should be the sent data
+    assert.equal(data1.body, "this is a content");
+    assert.equal(data2.body, "this is a content");
+    assert.equal(data3.body, "this is a content");
+
+    // Close the piping server
+    await closePromise(pipingServer);
+  });
 });
