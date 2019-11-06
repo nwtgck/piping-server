6e54d9141f1fa7966bba5a5c0d2768f2a5d8194b
Ryo Ota <nwtgck@gmail.com>
2019-11-06 12:22:22 +0900

    [change] Use Optional chaining instead of opt()

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,9 @@
 /**
  * Type which has optional property
  */
-type OptionalProperty<T> = {
+export type OptionalProperty<T> = {
   [K in keyof T]: T[K] | undefined;
 };
-
-/**
- * Optional property
- * @param obj
- */
-export function opt<T>(obj: T | null | undefined): OptionalProperty<T> {
-  return obj || ({} as OptionalProperty<T>);
-}
 
 /**
  * Mapping for optional
@@ -23,6 +15,7 @@ export function optMap<T, S>(
     f: (p: T, ...args: any[]) => S, obj: T | null | undefined, ...args: any[]
 ): OptionalProperty<S> {
   if (obj === null || obj === undefined) {
+    // FIXME: This is not safe when S is not an object.
     return {} as OptionalProperty<S>;
   } else {
     return f(obj, ...args);
